25f6abbc929235dc2a5cab882b98eaaed39edae8
fernandog197 <bluewavesalta@gmail.com>
2024-10-13 20:31:25 -0300

    Recuperacion y busqueda de contactos

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function doGet() {
 function getHTML(file) {
     return HtmlService.createHtmlOutputFromFile(file).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('1NHfNfvtcgf55_2w7pQ6uYQ57yt_yV2CbBKD2TYIo8kw').getActiveSheet();
+
+    let datos = hoja.getDataRange().getValues();
+
+    return datos;
+}
